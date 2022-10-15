125ad9705822e6598e6fde1f7e38b1f515d31ef8
opensea <openseasdk@gmail.com>
2022-10-15 14:12:50 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,20 +7,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesWrapper from './config/Routes';
 import './less/index.less';
 import store from './redux/store';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <CookiesProvider allCookies={['data', []]}>
+    <CookiesProvider>
       <Router>
         <RoutesWrapper />
       </Router>
     </CookiesProvider>
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
