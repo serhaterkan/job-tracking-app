1481c12fe1917e19c3f0e85454f686ad63b38dfa
opensea <openseasdk@gmail.com>
2022-10-15 13:28:31 +0300

    git link added

--- src/layouts/default/Footer.jsx
+++ src/layouts/default/Footer.jsx
@@ -1,6 +1,15 @@
+import { GithubOutlined } from '@ant-design/icons';
 import { Layout } from 'antd';
 import React from 'react';
 
 export default function Header() {
-  return <Layout.Footer>created by serhaterkan github</Layout.Footer>;
+  return (
+    <Layout.Footer>
+      created by serhaterkan
+      <a href="https://github.com/serhaterkan/job-tracking-app" target="_blank" rel="noreferrer">
+        &nbsp;
+        <GithubOutlined /> github
+      </a>
+    </Layout.Footer>
+  );
 }
